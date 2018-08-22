550d7e864e2af4243e88297791f4c450cbc66810
theocareso <theocareso@gmail.com>
2018-08-22 08:20:34 -0300

    Classes criadas

---
+++ src/entities/HourContract.java
@@ -0,0 +1,47 @@
+package entities;
+
+import java.util.Date;
+
+public class HourContract {
+
+	private Date date;
+	private double valuePerHour;
+	private Integer hours;
+
+	public HourContract(Date date, double valuePerHour, Integer hours) {
+		super();
+		this.date = date;
+		this.valuePerHour = valuePerHour;
+		this.hours = hours;
+	}
+
+	public HourContract() {
+		super();
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Integer getHours() {
+		return hours;
+	}
+
+	public void setHours(Integer hours) {
+		this.hours = hours;
+	}
+
+	public double getValuePerHour() {
+		return valuePerHour;
+	}
+
+	public void setValuePerHour(double valuePerHour) {
+		this.valuePerHour = valuePerHour;
+	}
+
+	
+}
---
+++ src/entities/Worker.java
@@ -0,0 +1,81 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.enums.WorkerLevel;
+
+public class Worker {
+
+	private String name;
+	private WorkerLevel level;
+	private double baseSalary;
+	private Department department;
+	private List<HourContract> contracts = new ArrayList<>();
+
+	public Worker() {
+	}
+
+	public Worker(String name, WorkerLevel level, double baseSalary, Department department,
+			List<HourContract> contracts) {
+		this.name = name;
+		this.level = level;
+		this.baseSalary = baseSalary;
+		this.department = department;
+		this.contracts = contracts;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public WorkerLevel getLevel() {
+		return level;
+	}
+
+	public void setLevel(WorkerLevel level) {
+		this.level = level;
+	}
+
+	public double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public List<HourContract> getContracts() {
+		return contracts;
+	}
+
+	public void addContract(HourContract contract) {
+		contracts.add(contract);
+	}
+
+	public void removeContract(HourContract contract) {
+		contracts.remove(contracts);
+	}
+
+	public double income(int year, int month) {
+
+	double sum = baseSalary;
+	for() {
+		
+	}
+		
+	}
+
+}
